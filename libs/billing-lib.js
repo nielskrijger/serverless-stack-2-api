2dc3da19f75bd35c0df9b66a4f1c6ade8cdce2a8
Niels Krijger <niels@kryger.nl>
2019-04-03 11:33:31 +0200

    add unit tests

--- libs/billing-lib.js
+++ libs/billing-lib.js
@@ -1,9 +1,11 @@
 export function calculateCost(storage) {
-  const rate = storage <= 10
-    ? 4
-    : storage <= 100
-      ? 2
-      : 1;
+  let rate = 4;
+  if (storage > 10) {
+    rate = 2
+  }
+  if (storage > 100) {
+    rate = 1;
+  }
 
   return rate * storage * 100;
 }
